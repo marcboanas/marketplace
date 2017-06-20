91280dbb018a498315b2f5c2066d303e84d65ad7
Marc Boanas <marcjohnboanas@gmail.com>
2017-06-20 11:29:01 +0100

    StaticPages added

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,8 @@ Rails.application.routes.draw do
   resources :messages
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'users#index'
+  get 'static_pages/home'
+  get 'static_pages/help'
+  get 'static_pages/about'
+  root 'static_pages#home'
 end
