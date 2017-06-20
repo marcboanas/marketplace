19ae5403ee8deb7720b130fbb17b027772385f93
Marc Boanas <marcjohnboanas@gmail.com>
2017-06-20 12:15:32 +0100

    FullTitle Helper added

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+
+	# Returns a dynamic page title
+	def full_title(page_title = '')
+		base_title = "Neil Cochrane"
+		if page_title.empty?
+			base_title
+		else
+			page_title + " | " + base_title
+		end
+	end
 end
