a70c22960ff2521de3565154224851a26646bd5b
Marc Boanas <marcjohnboanas@gmail.com>
2017-06-20 10:19:11 +0100

    Message resource added

---
+++ app/models/message.rb
@@ -0,0 +1,4 @@
+class Message < ApplicationRecord
+	belongs_to :user
+	validates :content, length: { maximum: 140 }, presence: true
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ApplicationRecord
+	has_many :messages
+	validates :email, presence: true
+	validates :name, presence: true 
 end
